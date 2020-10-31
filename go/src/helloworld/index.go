575f2155423959dd6389c2a8d4f5c505bcbe66e6
test <123@qq.com>
2020-10-31 15:18:11 +0800

    go crud

--- go/src/helloworld/index.go
+++ go/src/helloworld/index.go
@@ -10,8 +10,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", test)
-	http.ListenAndServe("127.0.0.1:8000", nil)
+	http.HandleFunc("/", del)
+	http.ListenAndServe("127.0.0.1:7000", nil)
 
 }
 
@@ -37,4 +37,37 @@ func test(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(age)
 		fmt.Println(phone)
 	}
+}
+
+func insert(w http.ResponseWriter, r *http.Request) {
+	db, dbconn := sql.Open("mysql", "root:root@(127.0.0.1:3306)/test")
+	//	fmt.Println(dbconn)
+	if dbconn != nil {
+		fmt.Println("query error")
+		os.Exit(1)
+	}
+	result, err := db.Exec("INSERT INTO employinfo(uname,age,phone,address)VALUES(?,?,?,?)", "gotest", "100", "13212345678", "北京市朝阳区")
+	if err != nil {
+		fmt.Println("insert failed,", err)
+	}
+	userId, err := result.LastInsertId()   //获取添加数据的id
+	rowCount, err := result.RowsAffected() //影响行数
+	fmt.Println("user_id:", userId)
+	fmt.Println("rowCount:", rowCount)
+}
+
+func del(w http.ResponseWriter, r *http.Request) {
+	db, dbconn := sql.Open("mysql", "root:root@(127.0.0.1:3306)/test")
+	//	fmt.Println(dbconn)
+	if dbconn != nil {
+		fmt.Println("query error")
+		os.Exit(1)
+	}
+
+	result, err := db.Exec("delete from employinfo where id=23")
+	if err != nil {
+		fmt.Println("insert failed,", err)
+	}
+	rowCount, err := result.RowsAffected() //影响行数
+	fmt.Println("rowCount:", rowCount)
 }
