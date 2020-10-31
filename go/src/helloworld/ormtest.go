afcaa7465c47428cc11d25dfcfc4650ee9ad45c1
test <123@qq.com>
2020-10-31 17:13:23 +0800

    orm crud

--- go/src/helloworld/ormtest.go
+++ go/src/helloworld/ormtest.go
@@ -12,7 +12,15 @@ import (
 var err error
 var engin *gorose.Engin
 
+type employinfo struct {
+	uname   string
+	age     int
+	phone   string
+	address string
+}
+
 func init() {
+
 	engin, err = gorose.Open(&gorose.Config{Driver: "mysql", Dsn: "root:root@(127.0.0.1:3306)/test"})
 }
 
@@ -20,13 +28,31 @@ func DB() gorose.IOrm {
 	return engin.NewOrm()
 }
 func main() {
-	http.HandleFunc("/", test)
+	http.HandleFunc("/", del)
 	http.ListenAndServe("127.0.0.1:8000", nil)
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
+func sel(w http.ResponseWriter, r *http.Request) {
 	// orm链式操作,查询单条数据
 	res, _ := DB().Table("employinfo").Get()
-	// res 类型为 map[string]interface{}
 	fmt.Fprintln(w, res)
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+	var data = map[string]interface{}{"uname": "gotestname", "age": 120, "phone": "13312345678", "address": "天上人间"}
+	//	fmt.Fprintln(w, "插入数据是", employ)
+	id, err := DB().Table("employinfo").Data(data).Insert()
+	if err != nil {
+		fmt.Fprintln(w, "DB.Insert err:", err)
+	}
+	fmt.Fprintln(w, "插入了数据，id是：", id)
+}
+
+func del(w http.ResponseWriter, r *http.Request) {
+	//	var where map[string]interface{}{"id": 27}
+	res, err := DB().Table("employinfo").Where("id", 30).Delete()
+	if err != nil {
+		fmt.Fprintln(w, "DB.del err:", err)
+	}
+	fmt.Fprintln(w, "删除了数据", res)
 }
